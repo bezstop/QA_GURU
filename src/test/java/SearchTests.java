54ffa3d4f7da922feb9b48bb450e0feaf4ee4b1a
Yuri Schiryaev <yuri-valerevich@yandex.ru>
2023-10-29 18:50:39 +0300

    add 1 homework for checking

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @BeforeEach
+    public void init() {
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    public static void openUrl(String url) {
+        open(url);
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+    }
+
+    @Test
+    void successfulSearchTest() {
+        openUrl("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
